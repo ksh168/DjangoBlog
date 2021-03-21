426ecd3d595f3273e002e3378ce51c35bfc0ca66
ksh168 <mailkunalsharma99@gmail.com>
2021-03-21 17:24:57 +0000

    Updated

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views		# . means current directory
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+]

cad3e532314cb5999b8c86b6be506d38e70aa0ec
ksh168 <mailkunalsharma99@gmail.com>
2021-03-25 05:29:41 +0000

    Added about page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse  #no longer used since we're using render to display html files
 
 def home(request):
     return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')

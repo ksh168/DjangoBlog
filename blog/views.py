71ddfec4bd8df12d97e7cf2ab5658ab84274851c
ksh168 <mailkunalsharma99@gmail.com>
2021-03-25 06:11:19 +0000

    added css and used inheritance

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 # from django.http import HttpResponse  #no longer used since we're using render to display html files
 
+posts = [
+    {
+        'author': 'person1',
+        'title': 'blog post1',
+        'content': 'First post',
+        'date_posted': '25/3/21'
+    },
+    {
+        'author': 'person2',
+        'title': 'blog post2',
+        'content': 'Second post',
+        'date_posted': '25/3/21'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
